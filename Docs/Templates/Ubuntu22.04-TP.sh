58ad670d2f12b53233f6028d345d47e38d7c343c
Lương Văn Công <luongvancong070129@gmail.com>
2023-10-05 10:07:23 +0700

    Update Ubuntu22.04-TP.sh

--- Docs/Templates/Ubuntu22.04-TP.sh
+++ Docs/Templates/Ubuntu22.04-TP.sh
@@ -53,22 +53,8 @@ sudo rm -f /etc/ssh/ssh_host_*
 # add check for ssh keys on reboot...regenerate if neccessary
 cat << 'EOL' | sudo tee /etc/rc.local
 #!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-
-
 test -f /etc/ssh/ssh_host_dsa_key || dpkg-reconfigure openssh-server
 
-#!/bin/sh
 id | grep root 1>/dev/null 2>&1
 if test 0 = 0; then
    growpart /dev/sda 3 > /dev/null 2>&1
